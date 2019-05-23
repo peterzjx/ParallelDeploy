1850f891dbb02f0530316b0447b572fced5d1dd0
peterzjx <peterzjx@gmail.com>
2019-05-22 22:12:46 -0400

    server end support

--- main.py
+++ main.py
@@ -1,61 +1,101 @@
+import datetime
 import numpy as np
+import itertools
 import json
-import subprocess
+import os
+import re
 
-def extend(param_space, new_var, new_choice):
-    """
-    :param param_space: list of dicts, the previous space of all parameters
-    :param new_var:
-    :param new_choice: list of values, the current parameter space for new_var
-    :return: same format as param_space
-    """
-    new_space = []
-    for item in param_space:
-        for value in new_choice:
-            new_item = dict(item)
-            new_item[new_var] = value
-            new_space.append(new_item)
-    return new_space
+def timestamp():
+    return datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
-def parse(config_path):
+def generate_param_space(vars):
     """
-    :param config_path: path to the configuration file
+    :param vars: dict of {var: option}
     :return: a list of dicts, each is a combination of parameter set
     """
-    with open(config_path) as config_file:
-        config = json.load(config_file)
-        task_name = config["task_name"]
-        command = config["command"]
-        assert("param" in config)
-        param_space = [dict()]
-        for var in config["param"]:
-            choice = []
-            varlist = config["param"][var]
-            if type(varlist) is dict:
-                if "linspace" in varlist:
-                    lspace = varlist["linspace"]
-                    assert(len(lspace) == 3)
-                    choice = np.linspace(*lspace)
-                elif "arange" in varlist:
-                    arange = varlist["arange"]
-                    choice = np.arange(*arange)
+    values_list = []
+    for var in vars:
+        values = []
+        value_option = vars[var]
+        if type(value_option) is dict:
+            if "linspace" in value_option:
+                lspace = value_option["linspace"]
+                assert(len(lspace) == 3)
+                values = np.linspace(*lspace)
+            elif "arange" in value_option:
+                arange = value_option["arange"]
+                values = np.arange(*arange)
+            elif "Table" in value_option:
+                arange = value_option["Table"]
+                assert(len(arange) == 3)
+                arange[1] += arange[2]
+                values = np.arange(*arange)
+        else:
+            if type(value_option) is list:
+                values = value_option
             else:
-                if type(varlist) is list:
-                    choice = varlist
-                else:
-                    choice = [varlist]
-            param_space = extend(param_space, var, choice)
-    return task_name, command, param_space
+                values = [value_option]
+        values_list.append(values)
+    param_space = [dict(zip(vars, choice)) for choice in itertools.product(*values_list)]
+    return param_space
 
-def call(command, param):
-    theproc = subprocess.Popen([command, param], shell=True)
-    theproc.communicate()
+def create_param_file(output_dir, unique_name, param):
+    directory = os.path.join(output_dir, unique_name)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    param_file = os.path.join(directory, "param.json")
+    with open(param_file, 'w') as f:
+        json.dump(param, f)
+    return param_file
+
+def submit(job_name, commands, work_dir, save_path, program, pbs, param_file):
+    queue_name = pbs["queue_name"]
+    wall_time = pbs["wall_time"]
+    command = """qsub - << EOJ
+#!/bin/bash -l
+#PBS -A {}
+#PBS -N {}
+#PBS -l nodes=1:ppn=1,mem=12gb
+#PBS -l walltime={}
+#PBS -j oe
+#PBS -o stdout.o
+{}
+cd {}
+{} {} {}
+EOJ""".format(queue_name, job_name, wall_time, commands, work_dir, program, param_file, save_path)
+    submit_message = os.popen(command).read()
+    try:
+        job_id = re.match('(\d+)\.torque', submit_message).group(1)
+    except:
+        job_id = ""
+    return job_id
 
 def main(config_path):
-    task_name, command, param_space = parse(config_path)
-    for choice in param_space:
-        param = str(choice).replace('\'', '\"')
-        call(command, param)
+    with open(config_path) as config_file:
+        config = json.load(config_file)
+        assert ("param" in config)
+        vars = config["param"]
+        param_space = generate_param_space(vars)
+        id_len = len(str(len(param_space)))
+        task_name = config["task_name"]
+        commands = "\n".join(config["commands"])
+        program = config["program"]
+        pbs = config["pbs"]
+        work_dir = config["work_dir"]
+        output_dir = os.path.join(work_dir, task_name)
+        if not os.path.exists(work_dir):
+            os.makedirs(work_dir)
+        unique_name_prefix = timestamp()
+        for i, param in enumerate(param_space):
+            os.chdir("/")
+            unique_name = unique_name_prefix + str(i).zfill(id_len)
+            job_name = task_name + str(i).zfill(id_len)
+            param_file = create_param_file(output_dir, unique_name, param)
+            save_path = os.path.join(output_dir, unique_name)
+            os.chdir(save_path)
+            job_id = submit(job_name, commands, work_dir, save_path, program, pbs, param_file)
+            print(job_id + " submitted")
+            # delete param_file?
 
 if __name__ == "__main__":
     # call("run.py", '{"var1":20, "var2":3}')
--- run.py
+++ run.py
@@ -1,26 +1,11 @@
 import json
 import sys
+import os
 
-def call(*args):
-    """
-    Simulate a subprocess with unnamed arguments
-    :param args:
-    :return: None
-    """
-    return sum(args)
-
-
-def main(command_str):
-    """
-    Calls the subprocess with a json string
-    :param command_str: json, with format "var": val
-    :return: None
-    """
-    cmd = json.loads(command_str)
-    print("Subprocess running with parameters ", cmd)
-    args = [v for k, v in sorted(cmd.items())]
-    print("Sum is", call(*args))
-
-if __name__ == "__main__":
-    command_str = sys.argv[1]
-    main(command_str)
+with open(sys.argv[1]) as f:
+    param = json.load(f)
+    args = [v for k, v in sorted(param.items())]
+    print(sum(args))
+    
+with open(os.path.join(sys.argv[2], "output.txt"), 'w') as f:
+    f.write("Output = " + str(sum(args)))
